4042ae6de5b21548f35c4499d6de70d9da9e7b63
fy <ercyao@qq.com>
2019-02-18 11:27:00 +0800

    打包文件

--- config/index.js
+++ config/index.js
@@ -57,10 +57,10 @@ module.exports = {
     dist_without_commonchunk,
     public_path,
     // // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../contest/index.html'),
 
     // // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../contest'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
